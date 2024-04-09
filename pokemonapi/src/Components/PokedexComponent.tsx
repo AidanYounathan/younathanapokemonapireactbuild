8984ae40dada6df9af891f7d3094e003d8725710
Aidan Younathan <ayounathan05@gmail.com>
2024-04-09 09:38:34 -0700

    Yesterdays work

--- pokemonapi/src/Components/PokedexComponent.tsx
+++ pokemonapi/src/Components/PokedexComponent.tsx
@@ -1,22 +1,111 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Components/PokedexComponent.css'
-import favhrt from "./Assets/Vector.png"
+import unfavhrt from "./Assets/Vector.png"
+import favhrt from "./Assets/favorited.png"
 import bulb from "./Assets/1 2.png"
+import { Evolution, Pokemon, RegEvolution } from '../DataServices/Interfaces/Interfaces'
+import { getAPI, pokeData, pokeDataEvo } from '../DataServices/DataServices'
 
 
 
 
 const PokedexComponent = () => {
 
-const [userInput, setUserInput] = useState<string>("Bulbasaur");
-const [Img, setImg] = useState<string>("favhrt");
+  const [userInput, setUserInput] = useState<string>("Bulbasaur");
+
+
+
+
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [location, setLocation] = useState<Location>();
+  const [evoData, setEvoData] = useState<Evolution>();
+  const [imgSrc, setImgSrc] = useState<string>("");
+  const [pokemonEvolution, setPokemonEvolution] = useState<RegEvolution | null>(null);
+  const [pokemonEvoData, setPokeEvoData] = useState<string[]>([]);
+  const [evolutionDatas, setEvolutionData] = useState<{ evolutionImage: string, evolutionId: string }[]>([])
+  const [favorite, setFavorite] = useState<string>(unfavhrt);
+  const [favorites, setFavorites] = useState<Pokemon[]>([]);
+  
+
+  useEffect(() => {
+    const getData = async () => {
+        const pokemonData = await pokeData(userInput);
+        const data: Pokemon = pokemonData;
+
+        const locationData = await getAPI(data.location_area_encounters);
+        const locData: Location = locationData;
+
+        const evolutionData = await pokeDataEvo(userInput);
+        const evoData: Evolution = evolutionData;
+        console.log(evoData.evolution_chain.url);
+        const evoTypeData = await getAPI(evoData.evolution_chain.url);
+        const evoType: {evolution_chain:{chain:{species:{name:string}; evolves_to:{species:{name:string}[]}[]}}}| any | RegEvolution = evoTypeData;
+        console.log(evoType);
+        setPokemonEvolution(evoType);
+        setEvoData(evoData);
+        setLocation(locData);
+        setPokemon(data);
+        
+        
+        const favorites = getLocalStorage();
+        const isFavorite = favorites.some((favPokemon: Pokemon) => favPokemon.name === userInput || String(favPokemon.id) === userInput);
+        if (isFavorite) {
+            setFavorite(favhrt);
+        } else {
+            setFavorite(unfavhrt);
+        }
+        const pokemonEvolutionChain: string[] = [];
+        if (evoType && evoType.chain) {
+            pokemonEvolutionChain.push(evoType.chain.species.name);
+            evoType.chain.evolves_to.forEach((e: {species:{name:string;}; evolves_to: string[];}) => {
+                e.species && pokemonEvolutionChain.push(e.species.name);
+                e.evolves_to.forEach((e: any) => {
+                    e.species && pokemonEvolutionChain.push(e.species.name);
+                });
+            });
+        }
+        setPokeEvoData(pokemonEvolutionChain);
+        console.log(pokemonEvolutionChain);
+    };
+    const favoritesData = getLocalStorage();
+    setFavorites(favoritesData);
+    console.log(favorites)
+    getData();
+
+}, [userInput, favorite]);
+
+
+const getLocalStorage = () => {
+  let localStorageData = localStorage.getItem("Favorites");
+  if (localStorageData == null) {
+      return [];
+  }
+  return JSON.parse(localStorageData);
+}
+
+
+  const CapitalFirstLetter = (userInput: string) => {
+    if (!userInput) return "";
+
+    let uncapped = userInput.split("-");
+    let cappedWords = uncapped.map(uncapped => uncapped.charAt(0).toUpperCase() + uncapped.slice(1));
+    let formattedInput = cappedWords.join(" ");
+
+    return formattedInput;
+  }
+
+
+
+
+
+
 
 
 
 
   return (
     <div className=' h-[100vh] bg-slate-600 m-3 md:m-8 flex justify-center'>
-      <div className='mb-3'>
+      <div className='mb-6'>
         <p className="text-center title text-6xl mt-4 md:text-8xl">One Dex</p>
 
         <div className="mb-5 mt-11 flex justify-center">
@@ -34,9 +123,9 @@ const [Img, setImg] = useState<string>("favhrt");
           <div className='flex'>
 
 
-            <div className=""><p id="pokeName" className="text-4xl md:text-5xl mt-5">Bulbasaur</p></div>
-            <div className="favEmpt "><img id="favBtn" className="h-[25px] md:h-[33px] flex ml-5" src={Img} alt="favorite pokemon btn" /></div>
-            
+            <div className=""><p id="pokeName" className="text-4xl md:text-5xl mt-5">{pokemon && pokemon ? CapitalFirstLetter(pokemon.name) : "Bulbasaur"}</p></div>
+            <div className="favEmpt "><img id="favBtn" className="h-[25px] md:h-[33px] flex ml-5" src={favorite} alt="favorite pokemon btn" /></div>
+
           </div>
         </div>
         <div>
